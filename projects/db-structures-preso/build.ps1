e84d514c265a06a73a18f98553e5b54a69be40a6
James Clark <sbj@dimins.com>
2017-05-20 23:42:05 -0400

    Nearly done with db structures presentation

--- projects/db-structures-preso/build.ps1
+++ projects/db-structures-preso/build.ps1
@@ -1,3 +1,9 @@
-copy -recurse node_modules\reveal.js\js lib
-copy -recurse node_modules\reveal.js\css lib
-copy -recurse node_modules\reveal.js\plugin lib
+del -recurse lib\reveal.js\*
+del -recurse lib\c3.js\*
+del -recurse lib\d3.js\*
+copy -recurse node_modules\reveal.js\js lib\reveal.js
+copy -recurse node_modules\reveal.js\css lib\reveal.js
+copy -recurse node_modules\reveal.js\plugin lib\reveal.js
+copy node_modules\c3\c3.js lib\c3.js
+copy node_modules\c3\c3.css lib\c3.js
+copy node_modules\d3\d3.js lib\d3.js
