7d1623ec44789478ff45c2544510d9d3cae1800c
James Clark <jameswclark@gmail.com>
2019-05-22 09:26:58 -0400

    escape room namer draft

---
+++ projects/escape-room-namer/fuse.js
@@ -0,0 +1,10 @@
+const { FuseBox } = require("fuse-box");
+
+const fuse = FuseBox.init({
+  homeDir: "src",
+  output: "dist/$name.js",
+});
+
+fuse.bundle("ern").instructions(`> ern.ts`);
+
+fuse.run();
