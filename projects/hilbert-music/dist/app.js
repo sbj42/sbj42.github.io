9a97f1e63ab0d4c2c6fec19893cb7c0186f660e1
James Clark <jameswclark@gmail.com>
2021-01-18 16:24:08 -0500

    update hilbert-music project

--- projects/hilbert-music/dist/app.js
+++ projects/hilbert-music/dist/app.js
@@ -11,7 +11,7 @@ require("./vis2d");
 if (!('AudioContext' in window)) {
     document.getElementById('desc').innerHTML += '<br/><br/><b>Your browser doesn\'t support the Web Audio API.';
 }
-//# sourceMappingURL=index.js.map
+
 });
 ___scope___.file("resolution.js", function(exports, require, module, __filename, __dirname){
 
@@ -30,7 +30,7 @@ input.oninput = function () {
     event_1.onRestart(+input.value);
 };
 event_1.onRestart(+input.value);
-//# sourceMappingURL=resolution.js.map
+
 });
 ___scope___.file("event.js", function(exports, require, module, __filename, __dirname){
 
@@ -98,7 +98,7 @@ function onChord(vs) {
     audio_1.start(hz);
 }
 exports.onChord = onChord;
-//# sourceMappingURL=event.js.map
+
 });
 ___scope___.file("vis1d.js", function(exports, require, module, __filename, __dirname){
 
@@ -164,7 +164,7 @@ function highlight(d) {
     vis1DColor(d, 'yellow');
 }
 exports.highlight = highlight;
-//# sourceMappingURL=vis1d.js.map
+
 });
 ___scope___.file("hilbert.js", function(exports, require, module, __filename, __dirname){
 
@@ -237,7 +237,7 @@ function conv1to2(d) {
     return d2xy(width2d, d);
 }
 exports.conv1to2 = conv1to2;
-//# sourceMappingURL=hilbert.js.map
+
 });
 ___scope___.file("gradient.js", function(exports, require, module, __filename, __dirname){
 
@@ -255,7 +255,7 @@ function getColor(d) {
     return "rgb(" + color1 + ", " + color2 + ", " + color3 + ")";
 }
 exports.getColor = getColor;
-//# sourceMappingURL=gradient.js.map
+
 });
 ___scope___.file("vis2d.js", function(exports, require, module, __filename, __dirname){
 
@@ -347,7 +347,7 @@ function highlight(d) {
     vis2DColor(x, y, 'yellow');
 }
 exports.highlight = highlight;
-//# sourceMappingURL=vis2d.js.map
+
 });
 ___scope___.file("audio.js", function(exports, require, module, __filename, __dirname){
 
@@ -417,7 +417,7 @@ function stop() {
     }
 }
 exports.stop = stop;
-//# sourceMappingURL=audio.js.map
+
 });
 ___scope___.file("scale.js", function(exports, require, module, __filename, __dirname){
 
@@ -429,7 +429,7 @@ function getHertz(d) {
     return 261.626 * Math.pow(2, p);
 }
 exports.getHertz = getHertz;
-//# sourceMappingURL=scale.js.map
+
 });
 ___scope___.file("chords.js", function(exports, require, module, __filename, __dirname){
 
@@ -465,9 +465,459 @@ addButton('Fmaj', [0, 5, 9]);
 addButton('Gmaj', [2, 7, 11]);
 addButton('Gmin', [2, 7, 10]);
 addButton('Daug', [2, 6, 10]);
-//# sourceMappingURL=chords.js.map
+
 });
 });
+FuseBox.pkg("fusebox-hot-reload", {}, function(___scope___){
+___scope___.file("index.js", function(exports, require, module, __filename, __dirname){
+
+"use strict";
+/**
+ * @module listens to `source-changed` socket events and actions hot reload
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+var Client = require('fusebox-websocket').SocketClient;
+exports.connect = function (port, uri) {
+    if (FuseBox.isServer) {
+        return;
+    }
+    port = port || window.location.port;
+    var client = new Client({
+        port: port,
+        uri: uri,
+    });
+    client.connect();
+    client.on('source-changed', function (data) {
+        console.info("%cupdate \"" + data.path + "\"", 'color: #237abe');
+        /**
+         * If a plugin handles this request then we don't have to do anything
+         **/
+        for (var index = 0; index < FuseBox.plugins.length; index++) {
+            var plugin = FuseBox.plugins[index];
+            if (plugin.hmrUpdate && plugin.hmrUpdate(data)) {
+                return;
+            }
+        }
+        if (data.type === 'js') {
+            FuseBox.flush();
+            FuseBox.dynamic(data.path, data.content);
+            if (FuseBox.mainFile) {
+                try {
+                    FuseBox.import(FuseBox.mainFile);
+                }
+                catch (e) {
+                    if (typeof e === 'string') {
+                        if (/not found/.test(e)) {
+                            return window.location.reload();
+                        }
+                    }
+                    console.error(e);
+                }
+            }
+        }
+        if (data.type === 'css' && __fsbx_css) {
+            __fsbx_css(data.path, data.content);
+        }
+    });
+    client.on('error', function (error) {
+        console.log(error);
+    });
+};
+
+});
+return ___scope___.entry = "index.js";
+});
+FuseBox.pkg("fusebox-websocket", {}, function(___scope___){
+___scope___.file("index.js", function(exports, require, module, __filename, __dirname){
+
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var events = require('events');
+var SocketClient = (function () {
+    function SocketClient(opts) {
+        opts = opts || {};
+        var port = opts.port || window.location.port;
+        var protocol = location.protocol === 'https:' ? 'wss://' : 'ws://';
+        var domain = location.hostname || 'localhost';
+        this.url = opts.host || "" + protocol + domain + ":" + port;
+        if (opts.uri) {
+            this.url = opts.uri;
+        }
+        this.authSent = false;
+        this.emitter = new events.EventEmitter();
+    }
+    SocketClient.prototype.reconnect = function (fn) {
+        var _this = this;
+        setTimeout(function () {
+            _this.emitter.emit('reconnect', { message: 'Trying to reconnect' });
+            _this.connect(fn);
+        }, 5000);
+    };
+    SocketClient.prototype.on = function (event, fn) {
+        this.emitter.on(event, fn);
+    };
+    SocketClient.prototype.connect = function (fn) {
+        var _this = this;
+        console.log('%cConnecting to fusebox HMR at ' + this.url, 'color: #237abe');
+        setTimeout(function () {
+            _this.client = new WebSocket(_this.url);
+            _this.bindEvents(fn);
+        }, 0);
+    };
+    SocketClient.prototype.close = function () {
+        this.client.close();
+    };
+    SocketClient.prototype.send = function (eventName, data) {
+        if (this.client.readyState === 1) {
+            this.client.send(JSON.stringify({ event: eventName, data: data || {} }));
+        }
+    };
+    SocketClient.prototype.error = function (data) {
+        this.emitter.emit('error', data);
+    };
+    /** Wires up the socket client messages to be emitted on our event emitter */
+    SocketClient.prototype.bindEvents = function (fn) {
+        var _this = this;
+        this.client.onopen = function (event) {
+            console.log('%cConnected', 'color: #237abe');
+            if (fn) {
+                fn(_this);
+            }
+        };
+        this.client.onerror = function (event) {
+            _this.error({ reason: event.reason, message: 'Socket error' });
+        };
+        this.client.onclose = function (event) {
+            _this.emitter.emit('close', { message: 'Socket closed' });
+            if (event.code !== 1011) {
+                _this.reconnect(fn);
+            }
+        };
+        this.client.onmessage = function (event) {
+            var data = event.data;
+            if (data) {
+                var item = JSON.parse(data);
+                _this.emitter.emit(item.type, item.data);
+                _this.emitter.emit('*', item);
+            }
+        };
+    };
+    return SocketClient;
+}());
+exports.SocketClient = SocketClient;
+
+});
+return ___scope___.entry = "index.js";
+});
+FuseBox.pkg("events", {}, function(___scope___){
+___scope___.file("index.js", function(exports, require, module, __filename, __dirname){
+
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+if (FuseBox.isServer) {
+    module.exports = global.require("events");
+} else {
+    function EventEmitter() {
+        this._events = this._events || {};
+        this._maxListeners = this._maxListeners || undefined;
+    }
+    module.exports = EventEmitter;
+
+    // Backwards-compat with node 0.10.x
+    EventEmitter.EventEmitter = EventEmitter;
+
+    EventEmitter.prototype._events = undefined;
+    EventEmitter.prototype._maxListeners = undefined;
+
+    // By default EventEmitters will print a warning if more than 10 listeners are
+    // added to it. This is a useful default which helps finding memory leaks.
+    EventEmitter.defaultMaxListeners = 10;
+
+    // Obviously not all Emitters should be limited to 10. This function allows
+    // that to be increased. Set to zero for unlimited.
+    EventEmitter.prototype.setMaxListeners = function(n) {
+        if (!isNumber(n) || n < 0 || isNaN(n))
+            throw TypeError("n must be a positive number");
+        this._maxListeners = n;
+        return this;
+    };
+
+    EventEmitter.prototype.emit = function(type) {
+        var er, handler, len, args, i, listeners;
+
+        if (!this._events)
+            this._events = {};
+
+        // If there is no 'error' event listener then throw.
+        if (type === "error") {
+            if (!this._events.error ||
+                (isObject(this._events.error) && !this._events.error.length)) {
+                er = arguments[1];
+                if (er instanceof Error) {
+                    throw er; // Unhandled 'error' event
+                }
+                throw TypeError("Uncaught, unspecified \"error\" event.");
+            }
+        }
+
+        handler = this._events[type];
+
+        if (isUndefined(handler))
+            return false;
+
+        if (isFunction(handler)) {
+            switch (arguments.length) {
+                // fast cases
+                case 1:
+                    handler.call(this);
+                    break;
+                case 2:
+                    handler.call(this, arguments[1]);
+                    break;
+                case 3:
+                    handler.call(this, arguments[1], arguments[2]);
+                    break;
+                    // slower
+                default:
+                    args = Array.prototype.slice.call(arguments, 1);
+                    handler.apply(this, args);
+            }
+        } else if (isObject(handler)) {
+            args = Array.prototype.slice.call(arguments, 1);
+            listeners = handler.slice();
+            len = listeners.length;
+            for (i = 0; i < len; i++)
+                listeners[i].apply(this, args);
+        }
+
+        return true;
+    };
+
+    EventEmitter.prototype.addListener = function(type, listener) {
+        var m;
+
+        if (!isFunction(listener))
+            throw TypeError("listener must be a function");
+
+        if (!this._events)
+            this._events = {};
+
+        // To avoid recursion in the case that type === "newListener"! Before
+        // adding it to the listeners, first emit "newListener".
+        if (this._events.newListener)
+            this.emit("newListener", type,
+                isFunction(listener.listener) ?
+                listener.listener : listener);
+
+        if (!this._events[type])
+        // Optimize the case of one listener. Don't need the extra array object.
+            this._events[type] = listener;
+        else if (isObject(this._events[type]))
+        // If we've already got an array, just append.
+            this._events[type].push(listener);
+        else
+        // Adding the second element, need to change to array.
+            this._events[type] = [this._events[type], listener];
+
+        // Check for listener leak
+        if (isObject(this._events[type]) && !this._events[type].warned) {
+            if (!isUndefined(this._maxListeners)) {
+                m = this._maxListeners;
+            } else {
+                m = EventEmitter.defaultMaxListeners;
+            }
+
+            if (m && m > 0 && this._events[type].length > m) {
+                this._events[type].warned = true;
+                console.error("(node) warning: possible EventEmitter memory " +
+                    "leak detected. %d listeners added. " +
+                    "Use emitter.setMaxListeners() to increase limit.",
+                    this._events[type].length);
+                if (typeof console.trace === "function") {
+                    // not supported in IE 10
+                    console.trace();
+                }
+            }
+        }
+
+        return this;
+    };
+
+    EventEmitter.prototype.on = EventEmitter.prototype.addListener;
+
+    EventEmitter.prototype.once = function(type, listener) {
+        if (!isFunction(listener))
+            throw TypeError("listener must be a function");
+
+        var fired = false;
+
+        function g() {
+            this.removeListener(type, g);
+
+            if (!fired) {
+                fired = true;
+                listener.apply(this, arguments);
+            }
+        }
+
+        g.listener = listener;
+        this.on(type, g);
+
+        return this;
+    };
+
+    // emits a 'removeListener' event iff the listener was removed
+    EventEmitter.prototype.removeListener = function(type, listener) {
+        var list, position, length, i;
+
+        if (!isFunction(listener))
+            throw TypeError("listener must be a function");
+
+        if (!this._events || !this._events[type])
+            return this;
+
+        list = this._events[type];
+        length = list.length;
+        position = -1;
+
+        if (list === listener ||
+            (isFunction(list.listener) && list.listener === listener)) {
+            delete this._events[type];
+            if (this._events.removeListener)
+                this.emit("removeListener", type, listener);
+
+        } else if (isObject(list)) {
+            for (i = length; i-- > 0;) {
+                if (list[i] === listener ||
+                    (list[i].listener && list[i].listener === listener)) {
+                    position = i;
+                    break;
+                }
+            }
+
+            if (position < 0)
+                return this;
+
+            if (list.length === 1) {
+                list.length = 0;
+                delete this._events[type];
+            } else {
+                list.splice(position, 1);
+            }
+
+            if (this._events.removeListener)
+                this.emit("removeListener", type, listener);
+        }
+
+        return this;
+    };
+
+    EventEmitter.prototype.removeAllListeners = function(type) {
+        var key, listeners;
+
+        if (!this._events)
+            return this;
+
+        // not listening for removeListener, no need to emit
+        if (!this._events.removeListener) {
+            if (arguments.length === 0)
+                this._events = {};
+            else if (this._events[type])
+                delete this._events[type];
+            return this;
+        }
+
+        // emit removeListener for all listeners on all events
+        if (arguments.length === 0) {
+            for (key in this._events) {
+                if (key === "removeListener") continue;
+                this.removeAllListeners(key);
+            }
+            this.removeAllListeners("removeListener");
+            this._events = {};
+            return this;
+        }
+
+        listeners = this._events[type];
+
+        if (isFunction(listeners)) {
+            this.removeListener(type, listeners);
+        } else if (listeners) {
+            // LIFO order
+            while (listeners.length)
+                this.removeListener(type, listeners[listeners.length - 1]);
+        }
+        delete this._events[type];
+
+        return this;
+    };
+
+    EventEmitter.prototype.listeners = function(type) {
+        var ret;
+        if (!this._events || !this._events[type])
+            ret = [];
+        else if (isFunction(this._events[type]))
+            ret = [this._events[type]];
+        else
+            ret = this._events[type].slice();
+        return ret;
+    };
+
+    EventEmitter.prototype.listenerCount = function(type) {
+        if (this._events) {
+            var evlistener = this._events[type];
+
+            if (isFunction(evlistener))
+                return 1;
+            else if (evlistener)
+                return evlistener.length;
+        }
+        return 0;
+    };
+
+    EventEmitter.listenerCount = function(emitter, type) {
+        return emitter.listenerCount(type);
+    };
+
+    function isFunction(arg) {
+        return typeof arg === "function";
+    }
+
+    function isNumber(arg) {
+        return typeof arg === "number";
+    }
+
+    function isObject(arg) {
+        return typeof arg === "object" && arg !== null;
+    }
+
+    function isUndefined(arg) {
+        return arg === void 0;
+    }
+}
+
+});
+return ___scope___.entry = "index.js";
+});
+FuseBox.import("fusebox-hot-reload").connect(4444, "")
 
 FuseBox.import("default/index.js");
 FuseBox.main("default/index.js");
